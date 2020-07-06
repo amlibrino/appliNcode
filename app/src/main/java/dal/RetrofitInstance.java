673d54055cef617e74a55ad70b55a7400ce556d6
amlibrino <hamid.benchikh@gmail.com>
2020-07-06 13:21:53 +0200

    mise

---
+++ app/src/main/java/dal/RetrofitInstance.java
@@ -0,0 +1,21 @@
+package dal;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "https://dev.amorce.org/apicode/api_Ncode/";
+
+    //@SuppressWarnings("ALL")
+    public static Retrofit getRetrofitInstance(){
+        if(retrofit == null){
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
